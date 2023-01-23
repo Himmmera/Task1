aa6abfccc3ba7c02cdda799393ada5d4b305ddef
Neto test <kimirina0676@mail.ru>
2023-01-23 15:12:24 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 70;
+        int milePrice = 20;
+        int mile = ticketPrice / milePrice;
+
+        System.out.println((mile)+ "колличество мили начисленных за купленный билет");
+    }
+
+}
